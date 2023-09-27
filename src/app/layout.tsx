6595c02cde767abdfede15f32d2cbd4d7ca0c605
sdavisde <sdavisde@gmail.com>
2023-09-26 22:05:03 -0500

    add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from 'next'
-import { Urbanist } from 'next/font/google'
 import './globals.css'
 import Theme from '@components/verse/ThemeProvider'
+import { Urbanist } from 'next/font/google'
+import { Analytics } from '@vercel/analytics/react'
 
 const urbanist = Urbanist({ subsets: ['latin'], weight: ['100', '200', '300', '400', '500'] })
 
@@ -15,6 +16,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang='en'>
       <body className={urbanist.className}>
         <Theme>{children}</Theme>
+        <Analytics />
       </body>
     </html>
   )
