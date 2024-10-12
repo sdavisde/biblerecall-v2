0419f1a9aa8449ec92de29108109d9a1d0f19f14
vortexconfig <admin@vortexconfig.com>
2024-10-11 22:37:55 -0500

    remove beta banner

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -15,12 +15,12 @@ export default async function Home() {
       className='w-full min-h-[92%] flex flex-col items-center'
     >
       <div className='w-[95%] md:w-[70%] lg:w-[55%] flex flex-col items-center gap-6 my-6'>
-        <h1 className='flag relative w-[80%] h-12 text-md centered drop-shadow bg-red scale-90 text-center px-4 mb-4'>
+        {/* <h1 className='flag relative w-[80%] h-12 text-md centered drop-shadow bg-red scale-90 text-center px-4 mb-4'>
           This app is in beta testing, verses and functionality may break until release
-        </h1>
+        </h1> */}
         <div className='w-full h-12 flex ml-6'>
           <BookIcon className='w-10 fill-white' />
-          <h4 className='text-base centered ml-4'>My Verses</h4>
+          <h1 className='text-base centered ml-4'>My Verses</h1>
         </div>
         <hr className='w-full bg-darkGrey h-[2px]' />
         {!user && <Loginbox />}
